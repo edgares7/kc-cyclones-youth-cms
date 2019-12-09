df673801901547bb75bfa140d2bf3e1b07c98bf7
Edgar Esparza <edgar.i.esparza@gmail.com>
2019-12-08 19:45:31 -0600

    changed homepage to diplay error with website url

--- wp-content/themes/kccyclones/front-page.php
+++ wp-content/themes/kccyclones/front-page.php
@@ -3,5 +3,6 @@
  * This is the template for whatever the front page is set to
  */
 get_header(); ?>
-
+<h1>You've reached this page by mistake.</h1>
+<p>Please visit https://kccyclonesyouth.com to visit our website.</p>
 <?php get_footer();
